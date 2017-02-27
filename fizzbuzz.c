26ed2672430b92b27c527d2f5edd12127402ed63
Christiana Johnson <anajilly@gmail.com>
2017-02-27 12:19:36 -0800

    reworks deterministic primes testing, experimental
    This rework of the deterministic primes test, which is still a variant of the
    classic, simple, naive primes test (not, for instance, any variant of MKS), but
    this one stores primes more efficiently, giving the test a greater reach. A
    problem is that the Bignum library in use manages its memory privately, and
    automatically, which means that if we allocate too much memory for allocating
    mpz_t pointers, the Bignum library has a greater chance of failing an allocation,
    which will bring the processing to an end. What's a better way to manage this?
    
    As it is, the largest prime this particular implementation was able to generate
    as part of its testing feedstock, had 13 digits. This is a large-ish prime, for
    a naive algorithm, running for only a couple hours, but it is entirely
    inadequate for the task. This would allow primality tests of numbers with
    around 26 digits.  Fibonacci numbers grow so fast that the usefulness of this
    test is minimal.
    
    I'm checking this in to save the work, because it's interesting, but the final
    implementation should not include this more or less useless code.
    
    This change also adds debugging symbols to the fizzbuzz binary.

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -4,9 +4,11 @@
 #include <math.h>
 #include <ctype.h>
 #include <string.h>
+#include <assert.h>
 
 #include <gmp.h>
 
+#define ENABLE_DETERMINISTIC_PRIMES 1
 
 // the non-deterministic primality results have been checked up to Fibonnaci: 354224848179261915075
 
@@ -24,16 +26,18 @@ typedef struct {
 static prog_opts_t opts = {0};
 
 
+#ifdef ENABLE_DETERMINISTIC_PRIMES
 // for deterministic prime testing.  a bad implementation.
-static int is_deterministically_prime( mpz_t F_np1 );
+static int allocate_primes_list();
+static void free_primes_list();
+static int is_deterministically_prime( mpz_t F_n );
 static int is_prime__against_list( mpz_t candidate );
 static int generate_primes_up_to( mpz_t max );
-static void deallocate_primes();
-static void initialize_primes();
+#endif
 
 
 
-static void fibn_step( mpz_t F_np1, mpz_t F_n, mpz_t F_nm1 )
+static void fibn_step( mpz_t F_np1, mpz_t F_n, mpz_t F_nm1 )         // use 3 arguments to avoid initialization of mpz_t for each call
 {
     mpz_add( F_np1, F_n, F_nm1 );                                    // np1 is a placeholder only
     mpz_set( F_nm1, F_n );                                           // nm1 and n are used to track the sequence
@@ -65,6 +69,7 @@ static void run_checks_and_report( uint64_t n, mpz_t F_n )
         int passes = 50;                                             // mpz_probab_prime_p() doc says 50 is upper reasonable number
         int primeclass = mpz_probab_prime_p( F_n, passes );          // if primeclass == 1; P( F_n is composite ) <= 1/(4^passes)
 
+#ifdef ENABLE_DETERMINISTIC_PRIMES
         if( primeclass == 1 )                                        // primality is uknown. With 50 passes, however, the probability
         {                                                            // that it's not prime, according to docs, is ~ 7.9e-31
             if( opts.use_deterministic_primes )
@@ -73,6 +78,7 @@ static void run_checks_and_report( uint64_t n, mpz_t F_n )
                     primeclass = 2;                                  // classification is changed
             }
         }
+#endif
 
         switch( primeclass )
         {
@@ -102,9 +108,6 @@ static int execute_request( uint64_t n_F )
     mpz_init_set_ui( F_n, 1 );
     mpz_init( F_np1 );
 
-    if( opts.use_deterministic_primes )
-        initialize_primes();
-
     if( opts.zero_is_fib )
     {
         run_checks_and_report( n, F_nm1 );
@@ -121,11 +124,6 @@ static int execute_request( uint64_t n_F )
     mpz_clear( F_n );
     mpz_clear( F_np1 );
 
-    if( opts.use_deterministic_primes )
-    {
-        deallocate_primes();
-    }
-
     return 0;
 }
 
@@ -136,7 +134,9 @@ static void print_usage_and_exit()
     fprintf( stderr, "usage: %s [options] [<n>]\n", progname );
     fprintf( stderr, "  options:\n" );
     fprintf( stderr, "    -? | -h   print this message and exit\n" );
+#ifdef ENABLE_DETERMINISTIC_PRIMES
     fprintf( stderr, "    -d        use a deterministic prime number check\n" );
+#endif
     fprintf( stderr, "    -v        increase verbosity. -v may be specified many times\n" );
     fprintf( stderr, "    -z        use zero as the first Fibonacci number\n" );
     fprintf( stderr, "    -s3,5p    supress the report of 3 and 5 as primes\n" );
@@ -148,6 +148,7 @@ int main( int argc, char ** argv )
 {
     uint64_t n_F = (uint64_t)-1;                                     // max(u64) means print F_n without limit
     int i = 0;
+    int retval = 0;
 
     progname = argv[0];
 
@@ -176,67 +177,127 @@ int main( int argc, char ** argv )
     if( n_F < 1 )                                                    // treat a request for zero Fibonacci
         n_F = (uint64_t)-1;                                          // numbers as a no-limit request
 
-    if( opts.verbose > 1 && n_F < (uint64_t)-1 )
+    // here, we run quick checks to see how many primes we might need
+    // if a deterministic check is desired.  deterministic checks are
+    // not supported when the set of F_n is unbounded.
+    if( n_F < (uint64_t)-1 )
     {
         mpz_t expected_largest_fib;
         mpf_t expected_largest_fib_d;
+        mpz_t sqrt_expected_largest_fib;
 
         mpz_init( expected_largest_fib );
+        mpz_init( sqrt_expected_largest_fib );
         mpf_init_set_d( expected_largest_fib_d, 1.61803398875 );
+
         mpf_pow_ui( expected_largest_fib_d, expected_largest_fib_d, n_F - (2 + opts.zero_is_fib));
         mpz_set_f( expected_largest_fib, expected_largest_fib_d );
-        mpf_sqrt( expected_largest_fib_d, expected_largest_fib_d );
+        mpz_sqrt( sqrt_expected_largest_fib, expected_largest_fib );
+        mpz_add_ui( sqrt_expected_largest_fib, sqrt_expected_largest_fib, 1 );  // we don't want the truncate sqrt
 
-        gmp_printf( "approx(F_%u) will be approximately: %Zd.\nsqrt( approx(F_%u) ) == %Ff\n",
-                n_F, expected_largest_fib, n_F, expected_largest_fib_d );
+        if( opts.verbose > 1 )
+        {
+            gmp_printf( "approx(F_%u) will be approximately: %Zd.\nsqrt( approx(F_%u) ) == %Zd\n",
+                    n_F, expected_largest_fib, n_F, sqrt_expected_largest_fib );
+        }
+
+#ifdef ENABLE_DETERMINISTIC_PRIMES
+        if( opts.use_deterministic_primes && allocate_primes_list() )
+        {
+            fprintf( stderr, "error: failed to allocate primes list. disabling deterministic prime checking -- %m\n" );
+            opts.use_deterministic_primes = 0;
+        }
+#endif
 
         mpz_clear( expected_largest_fib );
         mpf_clear( expected_largest_fib_d );
+        mpz_clear( sqrt_expected_largest_fib );
     }
 
-    return execute_request( n_F );
+    retval = execute_request( n_F );
+
+#ifdef ENABLE_DETERMINISTIC_PRIMES
+    if( opts.use_deterministic_primes )
+        free_primes_list();
+#endif
+
+    return retval;
 }
 
 
 
+#ifdef ENABLE_DETERMINISTIC_PRIMES
 // this is a simple list struct made for naive but deterministic primality testing
-typedef struct s_mpz_list s_mpz_list_t;
-struct s_mpz_list
+typedef struct 
 {
-    s_mpz_list_t * next;
-    mpz_t v;
-};
-static s_mpz_list_t primes;
+    mpz_t comp;
+    mpz_t min;
+} mpz_touple_t ;
+static mpz_touple_t * primes = NULL;
+
+static int primes_slots_taken = 0;
+static int primes_maxed_out = 0;
 static mpz_t max_prime_in_list;
 
-static int is_deterministically_prime( mpz_t F_np1 )
+#define PRIMES_LIST_SIZE (4 * 1024 * 1024)
+
+static int allocate_primes_list()
+{
+    primes = malloc( PRIMES_LIST_SIZE * sizeof(mpz_touple_t) );
+    if( ! primes )
+        return -1;
+
+    primes_slots_taken = 0;
+    return 0;
+}
+
+static void free_primes_list()
+{
+    if( ! primes )
+        return;
+
+    while( primes_slots_taken > 0 )
+    {
+        primes_slots_taken--;
+        mpz_clear( primes[primes_slots_taken].min );
+        mpz_clear( primes[primes_slots_taken].comp );
+    }
+
+    free( primes );
+    primes = NULL;
+}
+
+static int is_deterministically_prime( mpz_t F_n )
 {
     int isprime = 0;
     static int first_run = 1;
-    static mpz_t F_np1_sqrt;
+    static mpz_t F_n_sqrt;
 
     if( first_run )
     {
-        mpz_init( F_np1_sqrt );                                      // doing this frequently is slow. so make it static
+        mpz_init( F_n_sqrt );                                        // doing this frequently is slow. so make it static
         first_run = 0;                                               // and deal with a tiny bit of uncool mess.
     }
 
-    if( mpz_root( F_np1_sqrt, F_np1, 2 ) )                           // if the result of this sqrt() isn't exact,
-        mpz_add_ui( F_np1_sqrt, F_np1_sqrt, 2 );                     // add two to round up, maintaining even/odd.
+    if( mpz_root( F_n_sqrt, F_n, 2 ) )                               // if the result of this sqrt() isn't exact,
+        mpz_add_ui( F_n_sqrt, F_n_sqrt, 2 );                         // add two to round up, maintaining even/odd.
 
-    if( mpz_cmp( max_prime_in_list, F_np1_sqrt ) < 0 )               // if the max is less than the sqrt, generate more primes.
+    if( mpz_cmp( max_prime_in_list, F_n_sqrt ) < 0 )                 // if the max is less than the sqrt, generate more primes.
     {
-        if( generate_primes_up_to( F_np1_sqrt ) )                    // if generate_primes_up_to returns an error, we maxed out memory
+        if( generate_primes_up_to( F_n_sqrt ) )                      // if generate_primes_up_to returns an error, we maxed out memory
         {
+            fprintf( stderr, "error: generate_primes_up_to() returned an error. freeing primes list and turning off deterministic primality checks -- %m\n" );
+            if( opts.verbose ) gmp_printf( "max deterministic prime found: %Zd\n",  max_prime_in_list );
+
             opts.use_deterministic_primes = 0;                       // so we need to stop trying deterministic checks
-            deallocate_primes();                                     // this list is no longer useful and is taking up space
+            free_primes_list();                                      // the primes list is no longer useful. free it
             return 0;
         }
     }
 
-    if( mpz_cmp( max_prime_in_list, F_np1_sqrt ) >= 0 )              // if the max no no less than the sqrt, do the deterministic check
+    if( mpz_cmp( max_prime_in_list, F_n_sqrt ) >= 0 )                // if the max no less than the sqrt, do the deterministic check
     {
-        if( is_prime__against_list( F_np1 ) )
+        if( is_prime__against_list( F_n ) )
             isprime = 1;
     }
 
@@ -247,101 +308,103 @@ static int is_deterministically_prime( mpz_t F_np1 )
 static int is_prime__against_list( mpz_t candidate )
 {
     int iscomposite = 0;
+    int i;
 
-    s_mpz_list_t * p = &primes;
+    static int first_run = 1;
+    static mpz_t candidate_sqrt;
+    static mpz_t gcd;
 
-    do {
-        iscomposite = mpz_divisible_p( candidate, p->v );
-        if( iscomposite )
-            p = p->next;
-    } while( iscomposite && p );
+    assert( primes != NULL );
+
+    if( first_run )
+    {
+        first_run = 0;
+        mpz_init( candidate_sqrt );
+        mpz_init( gcd );
+    }
+
+    mpz_add_ui( candidate_sqrt, candidate, 1 );
+    mpz_sqrt( candidate_sqrt, candidate_sqrt );
+
+    for( i = 0; ! iscomposite && i < primes_slots_taken && mpz_cmp( primes[i].min, candidate_sqrt ) < 0; i++ )
+    {
+        mpz_gcd( gcd, candidate, primes[i].comp );
+        iscomposite = ( mpz_cmp_ui( gcd, 1 ) > 0 );  // if gcd > 1
+    }
 
     return ! iscomposite;
 }
 
 static int generate_primes_up_to( mpz_t max )
 {
-    int primes_count = 0;
     int error = 0;
-    s_mpz_list_t * p = &primes;
+    int primes_added = 0;
+    mpz_t nextprime;
+
+    if( primes_maxed_out )
+        return -1;
+
+    mpz_init( nextprime );
+
+    if( primes_slots_taken == 0 )
+    {
+        mpz_init_set_ui( primes[primes_slots_taken].min, 2 );
+        mpz_init_set_ui( primes[primes_slots_taken].comp, 2*3*5*7*11*13 );
+
+        mpz_init_set_ui( max_prime_in_list, 13 );
+
+        primes_slots_taken++;
+    }
+
+    if( opts.verbose > 2 )
+        printf( "\n" );
 
     do {
-        if( mpz_cmp( max, p->v ) < 0 )
+        if( mpz_cmp( max, max_prime_in_list ) < 0 )
             break;
 
-        if( p->next )
+        mpz_nextprime( nextprime, max_prime_in_list );               // this probability of the result being composite is very low.
+
+        while( ! is_prime__against_list( nextprime ) )           // but we're generating a deterministic check list, so check it.
+            mpz_nextprime( nextprime, nextprime );
+
+        primes_added++;
+
+        if( 500 < mpz_sizeinbase( primes[primes_slots_taken-1].comp, 10 ) )  // approximately how many base-10 digits?
         {
-            p = p->next;
-            primes_count++;
+            if( primes_slots_taken >= PRIMES_LIST_SIZE )
+            {
+                primes_added--;
+                error = -1;
+            }
+            else
+            {
+                mpz_init_set( primes[primes_slots_taken].comp, nextprime );
+                mpz_init_set( primes[primes_slots_taken].min, nextprime );
+                mpz_init_set( max_prime_in_list, nextprime );
+                primes_slots_taken++;
+            }
         }
         else
         {
-            p->next = calloc( 1, sizeof(s_mpz_list_t) );
-            if( p->next )
-            {
-                s_mpz_list_t * np = p->next;
-
-                mpz_nextprime( np->v, p->v );                        // this probability of the result being composite is very low.
-                while( ! is_prime__against_list( np->v ) )           // but we're generating a deterministic check list, so check it.
-                    mpz_nextprime( np->v, np->v );
-
-                mpz_init_set( max_prime_in_list, np->v );            // once we get here, we're sure that our result is correct
-                p = np;
-
-                primes_count++;
-            }
-            else
-                error = -1;
+            mpz_mul( primes[primes_slots_taken-1].comp, primes[primes_slots_taken-1].comp, nextprime );
+            mpz_init_set( max_prime_in_list, nextprime );
         }
 
-        if( opts.verbose )
+        if( opts.verbose > 2 )
         {
-            printf( "\rprimes in list: %d", primes_count );
+            printf( "\rprimes added to list: %d", primes_added );
             fflush( stdout );
         }
     }
-    while( p && ! error );
+    while( ! error );
 
-    if( opts.verbose )
+    if( opts.verbose > 2 )
         printf( "\n" );
+
+    mpz_clear( nextprime );
 
     return error;
 }
 
-static void deallocate_primes()
-{
-    s_mpz_list_t * p = primes.next;
-
-    while( p )
-    {
-        s_mpz_list_t * n = p->next;
-
-        mpz_clear( p->v );
-        free( p );
-
-        if( n )
-            p = n;
-    }
-
-    mpz_clear( primes.v );
-    primes.next = NULL;  // list is empty
-}
-
-// just add the first 3 primes to our list.
-static void initialize_primes()
-{
-    s_mpz_list_t * p = &primes;
-
-    mpz_init_set_ui( p->v, 2 );
-
-    p->next = calloc( 1, sizeof(s_mpz_list_t) );
-    p = p->next;
-    mpz_init_set_ui( p->v, 3 );
-
-    p->next = calloc( 1, sizeof(s_mpz_list_t) );
-    p = p->next;
-    mpz_init_set_ui( p->v, 5 );
-
-    mpz_init_set( max_prime_in_list, p->v );
-}
-
+#endif /* #ifdef ENABLE_DETERMINISTIC_PRIMES */
